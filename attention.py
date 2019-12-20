52472d3f2ebb42ca476e9bb3ee067d3e4cd1ca37
Eduard Valera i Zorita <eduardvalera@gmail.com>
2019-12-20 16:47:36 +0100

    Implements relative encoding. Adds first implementation of BERT

--- attention.py
+++ attention.py
@@ -1,7 +1,18 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
+from torch.nn import init
+
+'''
+Aux functions
+'''
+def _init_matrix(*dims):
+   m = torch.Tensor(*dims)
+   #from torch.nn.Linear source
+   init.kaiming_uniform_(m, a=np.sqrt(5))
+   return m
+
 
 '''
 Embeddings
@@ -43,7 +54,7 @@ class MultiHeadedAttention(nn.Module):
 
       self.mask = mask
       
-   def forward(self, q, k, v, mask=None):
+   def forward(self, q, v, mask=None):
       ''' 
          q, k, v ~ (Batch, L, d_model)
       Qh, Kh, Vh ~ (Batch, h, d_model/h, L)
@@ -52,7 +63,7 @@ class MultiHeadedAttention(nn.Module):
                O ~ (Batch, L, d_model)
       '''
       Qh = self.Wq(q).transpose(-2,-1).contiguous().view(q.shape[0], self.h, -1, q.shape[-2])
-      Kh = self.Wk(k).transpose(-2,-1).contiguous().view(q.shape[0], self.h, -1, q.shape[-2])
+      Kh = self.Wk(q).transpose(-2,-1).contiguous().view(q.shape[0], self.h, -1, q.shape[-2])
       Vh = self.Wv(v).transpose(-2,-1).contiguous().view(q.shape[0], self.h, -1, q.shape[-2])
 
       # Scaled Dot Product Attention in h blocks QKh_b = dot(Qh_b^T, Kh_b) for all h (head) and b (batch)
@@ -80,6 +91,95 @@ class MultiHeadedAttention(nn.Module):
       # Layer norm and residual connection
       return self.ln(q + self.Wo(O))
 
+class RelativeMultiHeadedAttention(nn.Module):
+
+   def __init__(self, L, d_model, h, mask=None, dropout=0.1):
+      super(RelativeMultiHeadedAttention, self).__init__()
+      assert d_model % h == 0
+      self.L = L
+      self.h = h
+      self.d_model = d_model
+      self.mask = mask
+      R = PositionalEncoding(d_model)(torch.arange(L-1,-L,-1,dtype=torch.float32)) # Relative positional encodings
+      R = R.transpose(-2,-1).contiguous()
+      self.register_buffer('R', R)
+
+      # Linear transformations of embeddings
+      self.Wq = nn.Parameter(_init_matrix(h, d_model, d_model//h))
+      self.Wv = nn.Parameter(_init_matrix(h, d_model//h, d_model))
+      self.Wke = nn.Parameter(_init_matrix(h, d_model//h, d_model))
+      self.Wkr = nn.Parameter(_init_matrix(h, d_model//h, d_model))
+
+      # Position and content biases
+      self.cb = nn.Parameter(_init_matrix(1,h,1,d_model//h)) # Content bias
+      self.pb = nn.Parameter(_init_matrix(1,h,1,d_model//h)) # Position bias
+
+      # Output layers
+      self.do = nn.Dropout(p = dropout) if dropout > 0 else None      
+      self.Wo = nn.Linear(d_model, d_model)
+      self.ln = nn.LayerNorm(d_model)
+      
+   def _shift_b(self, b):
+      # Inspired by https://github.com/kimiyoung/transformer-xl/blob/44781ed21dbaec88b280f74d9ae2877f52b492a5/pytorch/mem_transformer.py#L194
+      z = torch.zeros(b.shape[0], b.shape[1], b.shape[2], 1, device=b.device)
+      b_pad = torch.cat((z,b), 3).view(b.shape[0], b.shape[1], b.shape[3]+1, b.shape[2])
+      return b_pad[:,:,1:].view_as(b)[:,:,:,:b.shape[2]]
+
+   
+   def forward(self, E, Ev, mask=None):
+      '''
+         Ev, E  ~  (Batch, L, d_model)
+             R  ~  (1, d_model, 2L-1)
+            Wq  ~  (h, d_model, d_model/h)
+    Wv,Wke,Wkr  ~  (h, d_model/h, d_model)
+        cb, pb  ~  (1, h, 1, d_model/h)
+             q  ~  (Batch, h, L, d_model/h)
+          v, k  ~  (Batch, h, d_model/h, L)
+             Q  ~  (1, h, d_model/h, 2L-1)
+             b  ~  (Batch, h, L, 2L-1)
+       A, D, B  ~  (Batch, h, L, L)
+            Oh  ~  (Batch, h, d_model/h, L)
+             O  ~  (Batch, L, d_model)
+      '''
+      q = torch.einsum('iykl,xjlm->ijkm', (E[:,None,:,:], self.Wq[None,:,:,:]))
+      k = torch.einsum('xjkl,iyml->ijkm', (self.Wke[None,:,:,:], E[:,None,:,:]))
+      v = torch.einsum('xjkl,iyml->ijkm', (self.Wv[None,:,:,:], Ev[:,None,:,:]))
+      Q = torch.einsum('ijk,xkl->ijl', (self.Wkr, self.R))[None,:,:,:]
+      b = torch.einsum('ijkl,xjlm->ijkm', (q, Q))
+      d = torch.einsum('ijkl,ijlm->ijkm', (self.pb, Q))
+      
+      # Attention matrix
+      A_a = torch.einsum('ijkl,ijlm->ijkm',(q,k))
+      A_b = self._shift_b(b)
+      
+      # Compute bias vector and replicate to row dimension L
+      A_c = torch.einsum('xjkl,ijlm->ijkm',(self.cb,k)).repeat(1,1,k.shape[-1],1)
+      A_d = self._shift_b(d.repeat(1,1,E.shape[-2],1))
+
+      # Attention matrix
+      A = A_a + A_b + A_c + A_d
+
+      if mask is not None:
+         A = A.masked_fill(mask == 0, float('-inf'))
+      elif self.mask is not None:
+         A = A.masked_fill(self.mask == 0, float('-inf'))
+
+      # Attention softmax
+      p_attn = F.softmax(A, dim=-1)
+
+      # Dropout to attention probabilities
+      if self.do is not None:
+         p_attn = self.do(p_attn)
+
+      # Apply attention to v
+      Oh = torch.einsum('ijkl,ijml->ijkm',(v,A))
+
+      # Concatenate attention output
+      O = Oh.view(Oh.shape[0], -1, Oh.shape[-1]).transpose(-2,-1)
+
+      # Layer norm and residual connection
+      return self.ln(Ev + self.Wo(O))
+
    
 class FeedForwardNet(nn.Module):
    def __init__(self, d_model, d_ffn):
@@ -106,7 +206,18 @@ class EncoderBlock(nn.Module):
       self.ffn       = FeedForwardNet(d_model, d_ffn)
       
    def forward(self, x, mask):
-      return self.ffn(self.attn(x,x,x,mask))
+      return self.ffn(self.attn(x,x,mask))
+
+
+class RelativeEncoderBlock(nn.Module):
+   def __init__(self, L, d_model, d_ffn, h, dropout=0.1):
+      super(RelativeEncoderBlock, self).__init__()
+      
+      self.attn      = RelativeMultiHeadedAttention(L, d_model, h, dropout=dropout)
+      self.ffn       = FeedForwardNet(d_model, d_ffn)
+      
+   def forward(self, x, mask):
+      return self.ffn(self.attn(x,x,mask))
 
    
 class DecoderBlock(nn.Module):
@@ -118,7 +229,68 @@ class DecoderBlock(nn.Module):
       self.ffn        = FeedForwardNet(d_model, d_ffn)
       
    def forward(self, x, vk):
-      y  = self.attn0(x,x,x)
-      z  = self.attn1(y,vk,vk)
+      y  = self.attn0(x,x)
+      z  = self.attn1(y,vk)
       return self.ffn(z)
 
+class RelativeDecoderBlock(nn.Module):
+   def __init__(self, L, d_model, d_ffn, h, mask, dropout=0.1):
+      super(RelativeDecoderBlock, self).__init__()
+      
+      self.attn0      = RelativeMultiHeadedAttention(L, d_model, h, mask=mask, dropout=dropout)
+      self.attn1      = RelativeMultiHeadedAttention(L, d_model, h, mask=None, dropout=dropout)
+      self.ffn        = FeedForwardNet(d_model, d_ffn)
+      
+   def forward(self, x, vk):
+      y  = self.attn0(x,x)
+      z  = self.attn1(y,vk)
+      return self.ffn(z)
+
+
+'''
+ALBERT
+'''
+
+class ALBERT(nn.Module):
+   def __init__(self, N, E, H, h, d_ffn, L, n_word, dropout=0.1, device=None):
+      super(ALBERT,self).__init__()
+
+      # Model parameters
+      self.N = N
+      self.E = E
+      self.H = H
+      self.h = h
+      self.d_ffn = d_ffn
+      # Text parameters
+      self.L = L
+      self.n_word
+      # Model device
+      self.device = device if device is not None else 'cuda' if torch.cuda.is_available() else 'cpu'
+
+      # Input Embedding Transformations
+      self.wrd_emb = nn.Sequential(nn.Embedding(n_word, E), nn.Linear(E, H)).to(self.device)
+
+      # Self-attention layers
+      self.attn    = [attention.RelativeEncoderBlock(L, H, d_ffn, h, dropout=dropout)].to(self.device) for _ in range(N)]
+
+      # Output softmax
+      self.outprob = nn.Sequential(nn.Linear(H, n_word), nn.Softmax(-1)).to(self.device)
+      
+
+   def forward(self, seq, mask):
+      x = self.wrd_emb(seq)
+      for att in self.attn:
+         x = att(x, mask)
+      return self.outprob(x)
+      
+
+class BertMLMHead(nn.Module):
+   def __init__(self):
+      super(BertMLMHead, self).__init__()
+      # Layers:
+      # Last attention output of BERT (size H).
+      # Linear HxH
+      # Activation
+      # LayerNorm
+      # Embedding Output (Linear+Bias), optionally Linear layer is weight-tied to input Embedding
+      
--- noise_attn.py
+++ noise_attn.py
@@ -2,12 +2,14 @@ import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.multiprocessing as mp
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Local modules
 import sequence
 import attention
+from lookahead import Lookahead
 
 # Avoid plot crash without X-server
 plt.switch_backend('agg')
@@ -18,7 +20,7 @@ Transformer
 '''
 
 class NoiseAttention(nn.Module):
-   def __init__(self, L, n_word, d_model, n_att, d_ffn, h, dropout=0.1, device=None):
+   def __init__(self, L, n_word, d_model, n_att, d_ffn, h, dropout=0.1, device=None, relative=True):
       super(NoiseAttention, self).__init__()
 
       # Parameters
@@ -27,44 +29,55 @@ class NoiseAttention(nn.Module):
       self.d_model = d_model
       self.h = h
       self.device = device if device is not None else 'cuda' if torch.cuda.is_available() else 'cpu'
+      self.relative = relative
 
       # Input Embedding Transformations
       self.src_emb = nn.Embedding(n_word, d_model).to(self.device)
-      self.emb_wgt = np.sqrt(d_model)
+      self.emb_wgt = None
+      self.pos_enc = None
 
-      # Positional encodings
-      self.pos_enc  = attention.PositionalEncoding(d_model)(torch.arange(L, dtype=torch.float32)).to(self.device)
+      if not relative:
+         # Positional encodings
+         self.pos_enc = attention.PositionalEncoding(d_model)(torch.arange(L, dtype=torch.float32)).to(self.device)
+         self.emb_wgt = np.sqrt(d_model)
+         self.selfattn = [attention.EncoderBlock(d_model, d_ffn, h, dropout=dropout).to(self.device) for _ in range(n_att)]
+      else:
+         # Instantiate Encoder and Decoder blocks
+         self.selfattn = [attention.RelativeEncoderBlock(L, d_model, d_ffn, h, dropout=dropout).to(self.device) for _ in range(n_att)]
       
-      # Instantiate Encoder and Decoder blocks
-      self.selfattn = [attention.EncoderBlock(d_model, d_ffn, h, dropout).to(self.device) for _ in range(n_att)]
 
       # Output Softmax
       self.outprob = nn.Sequential(nn.Linear(d_model, n_word), nn.Softmax(-1)).to(self.device)
 
-      # Training status
-      self.steps = 0
-
       
    def forward(self, seq, mask):
       E = self.src_emb(seq)
-      x = E*self.emb_wgt + self.pos_enc
+      if not self.relative:
+         E = E*self.emb_wgt + self.pos_enc
       for attn in self.selfattn:
-         x = attn(x, mask)
+         x = attn(E, mask)
       return self.outprob(x)
 
    
-   def optimize(self, seq, epochs, batch_size, ndel_func):
+   def optimize(self, seqs, epochs, batch_size, ndel_func, init_lr=.01, min_lr=.001, lr_exp=1.01):
 
       # Optimizer
-      opt = torch.optim.Adam(self.parameters(), lr=0.001)
-      opt_sched = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, factor=0.1, patience=10, verbose=True)
+      base_opt = torch.optim.Adam(self.parameters(), lr=init_lr**(1/lr_exp))
+      opt = Lookahead(base_opt, k=5, alpha=0.5)
+      opt = base_opt
+      #opt_sched = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, factor=0.1, patience=10, verbose=True)
 
       # Logs
       train_loss = np.zeros(epochs)
       test_loss  = np.zeros(epochs)
-      
+
       for e in torch.arange(epochs):
-         for x in seq.train_batches(batch_size=batch_size):
+         # Update LR
+         cur_lr = base_opt.param_groups[0]['lr']
+         if cur_lr > min_lr:
+            base_opt.param_groups[0]['lr'] = max(min_lr,cur_lr**lr_exp)
+
+         for x in seqs.train_batches(batch_size=batch_size):
             # Compute one-hot encoded targets
             target = sequence.to_onehot(x, seqs.n_symbols, dtype=torch.float32)
             # Apply random deletions
@@ -72,12 +85,13 @@ class NoiseAttention(nn.Module):
             # Attention
             y = self(xdel, None)
             loss = torch.nn.functional.binary_cross_entropy(y, target)
+            mem = torch.cuda.memory_allocated()/1024/1024
 
             # Update parameters
             opt.zero_grad()
             loss.backward()
             opt.step()
-
+            
             l = float(loss.detach().to('cpu'))
             train_loss[e] += l/(seqs.train_seq.shape[0]//batch_size)
 
@@ -94,10 +108,15 @@ class NoiseAttention(nn.Module):
             test_loss[e] = float(torch.nn.functional.binary_cross_entropy(y, target).to('cpu'))
 
          # Optimizer lr schedule
-         opt_sched.step(train_loss[e])
+         #opt_sched.step(train_loss[e])
 
          # Verbose epoch
-         print("[epoch {}] train_loss={} test_loss={}".format(e+1, 100*train_loss[:e+1].mean(), 100*test_loss[:e+1].mean()))
+         print("[epoch {}] train_loss={:.3f} test_loss={:.3f} memory={:.2f}MB".format(e+1, 100*train_loss[:e+1].mean(), 100*test_loss[:e+1].mean(), mem))
+
+         # Save model
+         if (e+1)%10 == 0:
+            torch.save(self, 'saved_models/delattn_model_epoch{}.torch'.format(e))
+            
 
       return train_loss, test_loss
       
@@ -111,20 +130,23 @@ if __name__ == "__main__":
       attmodel = NoiseAttention(
          L = seqs.seqlen,
          n_word = seqs.n_symbols,
-         d_model = 64,
+         d_model = 256,
          n_att = 4,
-         d_ffn = 256,
+         d_ffn = 1024,
          h = 8,
-        device = 'cuda'
+         relative=True,
+         device = 'cuda'
       )
 
       epochs = 300
       batch_size = 16
-      ndel = lambda: np.random.randint(10,30)
+      init_lr = .01
+      min_lr =.001
+      lr_exp = 1.01
+      ndel = lambda: np.random.randint(0,8)
       
-      train_loss, test_loss = attmodel.optimize(seqs, epochs=epochs, batch_size=batch_size, ndel_func=ndel)
-      
-      torch.save(gapatt, 'delattn_model.torch')
+      train_loss, test_loss = attmodel.optimize(seqs, epochs=epochs, batch_size=batch_size, ndel_func=ndel, init_lr=init_lr, min_lr=min_lr)
+      torch.save(attmodel, 'delattn_model.torch')
       
       plt.figure()
       plt.plot(train_loss)
